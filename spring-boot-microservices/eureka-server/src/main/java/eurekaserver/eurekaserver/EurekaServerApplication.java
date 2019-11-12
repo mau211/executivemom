edc838057c9323e5998d78de094f624b1819e876
mau211 <mariela.chavez.a@gmail.com>
2019-11-12 10:57:31 -0500

    created eureka and zuul gateway

--- spring-boot-microservices/eureka-server/src/main/java/eurekaserver/eurekaserver/EurekaServerApplication.java
+++ spring-boot-microservices/eureka-server/src/main/java/eurekaserver/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,5 @@
-package com.example.eurekaserver;
+package eurekaserver.eurekaserver;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-microservices/eureka-server/src/test/java/eurekaserver/eurekaserver/EurekaServerApplicationTests.java
+++ spring-boot-microservices/eureka-server/src/test/java/eurekaserver/eurekaserver/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.eurekaserver;
+package eurekaserver.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
