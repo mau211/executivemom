422aad889318abe6a2ba304be16edec510a25750
mau211 <mariela.chavez.a@gmail.com>
2019-11-12 23:44:01 -0500

    Resolved mom-api connection to eureka

---
+++ spring-boot-microservices/mom-todo-api/src/main/resources/db/migration/V1__data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'MotherTeresa', 'Take kids to practice', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'Stork', 'Shop back to school supplies', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'QueenVictoria', 'Make dentist appointment', sysdate(), false);
