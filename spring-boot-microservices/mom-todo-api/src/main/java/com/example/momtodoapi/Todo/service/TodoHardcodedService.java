fe13a2f99a4d3d363249c5a894839c684c955345
mau211 <mariela.chavez.a@gmail.com>
2019-11-13 21:34:36 -0500

    added new features to react and connected todos data from backend

---
+++ spring-boot-microservices/mom-todo-api/src/main/java/com/example/momtodoapi/Todo/service/TodoHardcodedService.java
@@ -0,0 +1,59 @@
+package com.example.momtodoapi.Todo.service;
+
+import com.example.momtodoapi.Todo.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class TodoHardcodedService {
+
+    private static List<Todo> todos = new ArrayList<>();
+    private static long idCounter = 0;
+
+    static {
+        todos.add(new Todo(++idCounter, "WilmaFlintstone", "Take Pebbles to the park", new Date(), false));
+        todos.add(new Todo(++idCounter, "WilmaFlintstone", "Buy Pebbles new clothes. She's growing so fast", new Date(), false));
+        todos.add(new Todo(++idCounter, "WilmaFlintstone", "Meet up with friends for me time", new Date(), false));
+    }
+
+    public List<Todo> findAll() {
+        return todos;
+    }
+
+    public Todo save(Todo todo) {
+        if(todo.getId()==-1 || todo.getId()==0) {
+            todo.setId(++idCounter);
+            todos.add(todo);
+        } else {
+            deleteById(todo.getId());
+            todos.add(todo);
+        }
+        return todo;
+    }
+
+    public Todo deleteById(long id) {
+        Todo todo = findById(id);
+
+        if (todo == null)
+            return null;
+
+        if (todos.remove(todo)) {
+            return todo;
+        }
+
+        return null;
+    }
+
+    public Todo findById(long id) {
+        for (Todo todo : todos) {
+            if (todo.getId() == id) {
+                return todo;
+            }
+        }
+
+        return null;
+    }
+}
--- todo-app/src/components/AuthenticationService.js
+++ todo-app/src/components/AuthenticationService.js
@@ -14,6 +14,14 @@ class AuthenticationService {
         if(user===null) return false
         return true
     }
+
+    getLoggedInUserName() {
+        // let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
+        let user = sessionStorage.getItem('authenticatedUser')
+        if (user === null) return ''
+        return user
+
+    }
 }
 
 export default new AuthenticationService()
