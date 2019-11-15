346c2f3366048d31e9f9e24e2cf45ac4aa5a0299
mau211 <mariela.chavez.a@gmail.com>
2019-11-15 10:02:40 -0500

    updated ip adresss from aws to react app api calls.

--- todo-app/src/api/todo/HelloWorldService.js
+++ todo-app/src/api/todo/HelloWorldService.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 class HelloWorldService {
   executeHelloWorldService(){
-    return axios.get('http://localhost:8081/hello-mom')
-    // return axios.get('http://18.189.189.51:8081/hello-mom')
+    // return axios.get('http://localhost:8081/hello-mom')
+    return axios.get('http://18.191.192.115:8081/hello-mom')
   }
   executeHelloWorldBeanService(){
-    return axios.get('http://localhost:8081/hello-mom-bean')
-    // return axios.get('http://18.189.189.51:8081/hello-mom-bean')
+    // return axios.get('http://localhost:8081/hello-mom-bean')
+    return axios.get('http://18.191.192.115:8081/hello-mom-bean')
 
   }
   executeHelloWorldPathVariableService(name){
-    return axios.get(`http://localhost:8081/hello-mom/path-variable/${name}`)
-    // return axios.get(`http://18.189.189.51:8081/hello-mom/path-variable/${name}`)
+    // return axios.get(`http://localhost:8081/hello-mom/path-variable/${name}`)
+    return axios.get(`http://18.191.192.115:8081/hello-mom/path-variable/${name}`)
 
   }
 }
